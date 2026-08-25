8ac7c817c09ccffa7ba69e55621ee111ad9fa56e
Tanmya Srivastava <Tanmya>
2026-08-25 13:19:34 -0700

    Add Question constructor and initialize values

--- Question.java
+++ Question.java
@@ -9,6 +9,15 @@ public class Question{
     private String opt4;
     private String ans;
 
+    public Question(String question, int id, String opt1, String opt2, String opt3, String opt4, String ans) {
+        this.question = question;
+        this.id = id;
+        this.opt1 = opt1;
+        this.opt2 = opt2;
+        this.opt3 = opt3;
+        this.opt4 = opt4;
+        this.ans = ans;
+    }
     public String getQuestion() {
         return question;
     }
